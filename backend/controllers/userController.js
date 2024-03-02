6b7b697b4e6376c623e7d0e71308a984d678e7ad
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-03-02 12:11:53 +0530

    feat: setup user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,4 +7,38 @@ const loginUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Login user" });
 });
 
-export { loginUser };
+// @desc    Register a new user
+// @route   POST /api/users/register
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register user" });
+});
+
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Public
+const logoutUser = (req, res) => {
+  res.status(200).json({ message: "Logout user" });
+};
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get user profile" });
+});
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update user profile" });
+});
+
+export {
+  loginUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
