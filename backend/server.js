dcce60b68b65e6cc28bd50754fe0aad911103065
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-03-05 11:00:02 +0530

    feat: setup MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,14 @@ import express from "express";
 import "dotenv/config";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+
+connectDB();
+
+app.get("/", (req, res) => res.send("Running"));
 
 app.use("/api/users", userRoutes);
 app.use(notFound);
