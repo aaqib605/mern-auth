18599e170bfe88432dbe624635727ea631395c5f
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-03-05 11:59:39 +0530

    feat: setup logout user endpoint and destroy cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,7 +61,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = (req, res) => {
-  res.status(200).json({ message: "Logout user" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "Logged out successfully" });
 };
 
 // @desc    Get user profile
