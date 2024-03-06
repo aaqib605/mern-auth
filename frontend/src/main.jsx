954749863a4f8f86c1547945d5e25bba38f57bcb
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-03-06 11:30:23 +0530

    feat: setup login & register form UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,12 +9,16 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen.jsx";
+import RegisterScreen from "./screens/RegisterScreen.jsx";
 import "./index.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
 );
